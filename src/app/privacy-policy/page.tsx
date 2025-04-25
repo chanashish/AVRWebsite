102bd45e136eef5e29d39ac99f0d51706359a6ce
Manas Tewari <manas@eazotel.com>
2025-04-25 19:41:55 +0530

    build error solved

--- src/app/privacy-policy/page.tsx
+++ src/app/privacy-policy/page.tsx
@@ -14,7 +14,7 @@ const page = () => {
                 />
             </Section>
             <SectionWithContainer>
-                <div className="flex flex-col gap-[40px]">
+            <div className="flex flex-col gap-[40px]">
                     <div className="flex flex-col gap-[24px]">
                         <h2 className="text-[32px] text-[#2F4B26] font-normal playfair">Consent</h2>
                         <p className="roboto text-[20px] text-[#686767] font-light">
@@ -52,17 +52,17 @@ const page = () => {
 
                     <div className="flex flex-col gap-[24px]">
                         <p className="roboto text-[20px] text-[#686767] font-light">
-                            Anand Vardhan Resort follows a standard procedure of using log files. These files log visitors when they visit websites. All hosting companies do this and are a part of hosting services' analytics. The information collected by log files include internet protocol (IP) addresses, browser type, Internet Service Provider (ISP), date and time stamp, referring/exit pages, and possibly the number of clicks. These are not linked to any information that is personally identifiable. The purpose of the information is for analyzing trends, administering the site, tracking users' movement on the website, and gathering demographic information.
+                            Anand Vardhan Resort follows a standard procedure of using log files. These files log visitors when they visit websites. All hosting companies do this and are a part of hosting services&apos; analytics. The information collected by log files include internet protocol (IP) addresses, browser type, Internet Service Provider (ISP), date and time stamp, referring/exit pages, and possibly the number of clicks. These are not linked to any information that is personally identifiable. The purpose of the information is for analyzing trends, administering the site, tracking users&apos; movement on the website, and gathering demographic information.
                         </p>
                     </div>
 
                     <div className="flex flex-col gap-[24px]">
                         <h2 className="text-[32px] text-[#2F4B26] font-normal playfair">Cookies and Web Beacons</h2>
                         <p className="roboto text-[20px] text-[#686767] font-light">
-                            Like any other website, Anand Vardhan Resort uses 'cookies'. These cookies are used to store information including visitors' preferences, and the pages on the website that the visitor accessed or visited. The information is used to optimize the users' experience by customizing our web page content based on visitors' browser type and/or other information.
+                            Like any other website, Anand Vardhan Resort uses &apos;cookies&apos;. These cookies are used to store information including visitors&apos; preferences, and the pages on the website that the visitor accessed or visited. The information is used to optimize the users&apos; experience by customizing our web page content based on visitors&apos; browser type and/or other information.
                         </p>
                         <p className="roboto text-[20px] text-[#686767] font-light">
-                            For more general information on cookies, please read "What Are Cookies" from Cookie Consent.
+                            For more general information on cookies, please read &quot;What Are Cookies&quot; from Cookie Consent.
                         </p>
                     </div>
 
@@ -79,7 +79,7 @@ const page = () => {
                             You may consult this list to find the Privacy Policy for each of the advertising partners of Anand Vardhan Resort.
                         </p>
                         <p className="roboto text-[20px] text-[#686767] font-light">
-                            Third-party ad servers or ad networks uses technologies like cookies, JavaScript, or Web Beacons that are used in their respective advertisements and links that appear on Anand Vardhan Resort, which are sent directly to users' browser. They automatically receive your IP address when this occurs. These technologies are used to measure the effectiveness of their advertising campaigns and/or to personalize the advertising content that you see on websites that you visit.
+                            Third-party ad servers or ad networks uses technologies like cookies, JavaScript, or Web Beacons that are used in their respective advertisements and links that appear on Anand Vardhan Resort, which are sent directly to users&apos; browser. They automatically receive your IP address when this occurs. These technologies are used to measure the effectiveness of their advertising campaigns and/or to personalize the advertising content that you see on websites that you visit.
                         </p>
                         <p className="roboto text-[20px] text-[#686767] font-light">
                             Note that Anand Vardhan Resort has no access to or control over these cookies that are used by third-party advertisers.
@@ -92,7 +92,7 @@ const page = () => {
                             Anand Vardhan Resort Privacy Policy does not apply to other advertisers or websites. Thus, we are advising you to consult the respective Privacy Policies of these third-party ad servers for more detailed information. It may include their practices and instructions about how to opt-out of certain options.
                         </p>
                         <p className="roboto text-[20px] text-[#686767] font-light">
-                            You can choose to disable cookies through your individual browser options. To know more detailed information about cookie management with specific web browsers, it can be found at the browsers' respective websites.
+                            You can choose to disable cookies through your individual browser options. To know more detailed information about cookie management with specific web browsers, it can be found at the browsers&apos; respective websites.
                         </p>
                     </div>
 
@@ -102,9 +102,9 @@ const page = () => {
                             Under the CCPA, among other rights, California consumers have the right to:
                         </p>
                         <ul className="list-disc pl-6 roboto text-[20px] text-[#686767] font-light space-y-2 pl-8">
-                            <li>Request that a business that collects a consumer's personal data disclose the categories and specific pieces of personal data that a business has collected about consumers.</li>
+                            <li>Request that a business that collects a consumer&apos;s personal data disclose the categories and specific pieces of personal data that a business has collected about consumers.</li>
                             <li>Request that a business delete any personal data about the consumer that a business has collected.</li>
-                            <li>Request that a business that sells a consumer's personal data, not sell the consumer's personal data.</li>
+                            <li>Request that a business that sells a consumer&apos;s personal data, not sell the consumer&apos;s personal data.</li>
                             <li>If you make a request, we have one month to respond to you. If you would like to exercise any of these rights, please contact us.</li>
                             <li>GDPR Data Protection Rights</li>
                         </ul>
@@ -112,21 +112,21 @@ const page = () => {
 
                     <div className="flex flex-col gap-[24px]">
                         <p className="roboto text-[20px] text-[#686767] font-light">
-                            You can choose to disable cookies through your individual browser options. To know more detailed information about cookie management with specific web browsers, it can be found at the browsers' respective websites.
+                            You can choose to disable cookies through your individual browser options. To know more detailed information about cookie management with specific web browsers, it can be found at the browsers&apos; respective websites.
                         </p>
                         <ul className="list-disc pl-6 roboto text-[20px] text-[#686767] font-light space-y-2 pl-8">
-                            <li>The right to access – You have the right to request copies of your personal data. We may charge you a small fee for this service.</li>
-                            <li>The right to rectification – You have the right to request that we correct any information you believe is inaccurate. You also have the right to request that we complete the information you believe is incomplete.</li>
-                            <li>The right to erasure – You have the right to request that we erase your personal data, under certain conditions.</li>
-                            <li>The right to restrict processing – You have the right to request that we restrict the processing of your personal data, under certain conditions.</li>
-                            <li>The right to object to processing – You have the right to object to our processing of your personal data, under certain conditions.</li>
-                            <li>The right to data portability – You have the right to request that we transfer the data that we have collected to another organization, or directly to you, under certain conditions.</li>
+                            <li>The right to access - You have the right to request copies of your personal data. We may charge you a small fee for this service.</li>
+                            <li>The right to rectification - You have the right to request that we correct any information you believe is inaccurate. You also have the right to request that we complete the information you believe is incomplete.</li>
+                            <li>The right to erasure - You have the right to request that we erase your personal data, under certain conditions.</li>
+                            <li>The right to restrict processing - You have the right to request that we restrict the processing of your personal data, under certain conditions.</li>
+                            <li>The right to object to processing - You have the right to object to our processing of your personal data, under certain conditions.</li>
+                            <li>The right to data portability - You have the right to request that we transfer the data that we have collected to another organization, or directly to you, under certain conditions.</li>
                             <li>If you make a request, we have one month to respond to you. If you would like to exercise any of these rights, please contact us.</li>
                         </ul>
                     </div>
 
                     <div className="flex flex-col gap-[24px]">
-                        <h2 className="text-[32px] text-[#2F4B26] font-normal playfair">Children's Information</h2>
+                        <h2 className="text-[32px] text-[#2F4B26] font-normal playfair">Children&apos;s Information</h2>
                         <p className="roboto text-[20px] text-[#686767] font-light">
                             Another part of our priority is adding protection for children while using the internet. We encourage parents and guardians to observe, participate in, and/or monitor and guide their online activity.
                         </p>
